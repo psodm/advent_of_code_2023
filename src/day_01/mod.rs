717a3570151cec0ada139cc8a073df3a5edc2e44
psodm <peter.stacey@broadcom.com>
2023-12-02 10:35:54 +1300

    provided idiomatic alternatives

--- src/day_01/mod.rs
+++ src/day_01/mod.rs
@@ -33,3 +33,56 @@ pub fn parts() {
     println!("D01-P1: {}", p1);
     println!("D01-P2: {}", p2);
 }
+
+// The following is an alternative version, with the parts split into 1 and 2 based on
+// a more idiomatic rewrite by u/AugustusLego
+// on the reddit megathread for day 1:
+// https://www.reddit.com/r/adventofcode/comments/1883ibu/2023_day_1_solutions/kbj8l1n/?context=3
+#[allow(unused)]
+pub fn solution_alternate_part1() {
+    let out: Vec<u32> = include_str!("../../input/day_01.txt")
+        .lines()
+        .filter_map(|line| {
+            let mut digits: Vec<char> = vec![];
+            line.chars().for_each(|c| {
+                if c.is_numeric() {
+                    digits.push(c);
+                }
+            });
+            Some(
+                format!("{}{}", digits.first()?, digits.last()?).parse::<u32>().unwrap()
+            )
+        })
+        .collect();
+    println!("D01-P1: {}", out.iter().sum::<u32>());
+}
+
+#[allow(unused)]
+pub fn solution_alternate_part2() {
+    let nums = vec![
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+    let out: Vec<u32> = include_str!("../../input/day_01.txt")
+        .lines()
+        .filter_map(|line| {
+            let mut digits: Vec<char> = vec![];
+            line.char_indices().for_each(|(i, c)| {
+                if c.is_numeric() {
+                    digits.push(c);
+                };
+                for (idx, val) in  nums.iter().enumerate() {
+                    if line[i..].starts_with(val) {
+                        match char::from_digit((idx+1).try_into().unwrap(), 10) {
+                            Some(num) => digits.push(num),
+                            None => ()
+                        }
+                    }
+                };
+            });
+            Some(
+                format!("{}{}", digits.first()?, digits.last()?).parse::<u32>().unwrap()
+            )
+        })
+        .collect();
+    println!("D01-P1: {}", out.iter().sum::<u32>());
+}
